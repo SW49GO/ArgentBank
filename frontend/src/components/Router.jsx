cc25514e8fcb970aaf290d88ed13f004a97208f5
SW49GO <gohard.so49@gmail.com>
2023-11-21 11:38:52 +0100

    React App Installation

---
+++ frontend/src/components/Router.jsx
@@ -0,0 +1,29 @@
+import {BrowserRouter , Routes, Route} from 'react-router-dom'
+import Login from '../pages/Login'
+import Error from '../pages/Error'
+import Home from '../pages/Home'
+import Profile from '../pages/Profile'
+
+/**
+ * Component function for routing
+ * @returns {JSX.Element}
+ */
+function Router(){
+
+    /**
+     * Function 
+     * @returns {JSX.Element}
+     */
+
+    return ( <BrowserRouter>
+                <Routes>
+                <Route exact path='/' element={<Home/>} />
+                <Route path='/login' element={<Login/>} />
+                <Route path='/profile' element={<Profile/>} />
+                <Route path='*' element={<Error/>}/>
+                </Routes>
+            </BrowserRouter>
+            )
+}
+
+export default Router
